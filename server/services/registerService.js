7a484a4a517e2f47cc3e0e3223344921c564b18d
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2021-02-23 20:09:07 -0700

    not inserting...

--- server/services/registerService.js
+++ server/services/registerService.js
@@ -4,15 +4,24 @@
 
 const fileService = require('./fileService');
 
- exports.addUser = ()=>{
-    const users = fileService.getFileContents('../data/users.json');
+const addUser = ()=>{
+  const users = fileService.getFileContents('../data/users.json');
    
-  users.forEach(user => {
-      const add  = fileService.writeFileContents(user)
-      return add
-  });
-
     
      
 }
 
+addUser({username:"pavlito", email:"pa@eeee.com", password:"1234567"});
+
+/* 
+    TEST DATA
+
+    pavs
+    pavs@gamil.com
+    123456
+
+    users.map(user => {
+    fs.writeFileSync(path.join(__dirname, '../data/users.json'), JSON.stringify(user));
+  });
+
+*/
